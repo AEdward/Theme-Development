41f0fdad6d9f35efda0b6689502ce36d6a62d7e4
AEdward <eanahom@yahoo.com>
2019-12-13 19:19:19 +0300

    link with js

--- inc/customizer.php
+++ inc/customizer.php
@@ -50,6 +50,6 @@ function ocean_wp_child_by_anahom_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function ocean_wp_child_by_anahom_customize_preview_js() {
-	wp_enqueue_script( 'ocean_wp_child_by_anahom-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'ocean_wp_child_by_anahom-customizer', get_template_directory_uri() . '/dist/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'ocean_wp_child_by_anahom_customize_preview_js' );
