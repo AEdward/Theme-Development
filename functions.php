ea3022e40e51c1b59f24b1378e4aac771c2e91c4
AEdward <eanahom@yahoo.com>
2019-12-13 19:17:49 +0300

    link with widgets,php and Boostrap navwalker

--- functions.php
+++ functions.php
@@ -164,6 +164,17 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * widgets File.
+ */
+require get_template_directory() . '/inc/widgets.php';
+
+/**
+ * Bootstrap navwalker file.
+ */
+require get_template_directory() . '/inc/Bootstrap-wp-navwalker.php';
+
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
